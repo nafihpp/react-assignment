234ef68aad7291988a8bb09c26dbad1b947181ba
nafihpp <nafihpp@gmail.com>
2023-01-02 18:33:31 +0530

    first commit

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-
-import { history } from '../_helpers';
-import { alertActions } from '../_actions';
-import { PrivateRoute } from '../_components';
-import { HomePage } from '../HomePage';
-import { LoginPage } from '../LoginPage';
-import { RegisterPage } from '../RegisterPage';
+import React from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { history } from "../_helpers";
+import { alertActions } from "../_actions";
+import { PrivateRoute } from "../_components";
+import { HomePage } from "../HomePage";
+import { LoginPage } from "../LoginPage";
+import { RegisterPage } from "../RegisterPage";
 
 class App extends React.Component {
     constructor(props) {
@@ -25,14 +24,23 @@ class App extends React.Component {
             <div className="jumbotron">
                 <div className="container">
                     <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
+                        {alert.message && (
+                            <div className={`alert ${alert.type}`}>
+                                {alert.message}
+                            </div>
+                        )}
                         <Router history={history}>
                             <Switch>
-                                <PrivateRoute exact path="/" component={HomePage} />
+                                <PrivateRoute
+                                    exact
+                                    path="/"
+                                    component={HomePage}
+                                />
                                 <Route path="/login" component={LoginPage} />
-                                <Route path="/register" component={RegisterPage} />
+                                <Route
+                                    path="/register"
+                                    component={RegisterPage}
+                                />
                                 <Redirect from="*" to="/" />
                             </Switch>
                         </Router>
@@ -49,7 +57,7 @@ function mapState(state) {
 }
 
 const actionCreators = {
-    clearAlerts: alertActions.clear
+    clearAlerts: alertActions.clear,
 };
 
 const connectedApp = connect(mapState, actionCreators)(App);
--- src/_helpers/fake-backend.js
+++ src/_helpers/fake-backend.js
@@ -1,21 +1,26 @@
 // array in local storage for registered users
-let users = JSON.parse(localStorage.getItem('users')) || [];
-    
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
 export function configureFakeBackend() {
     let realFetch = window.fetch;
     window.fetch = function (url, opts) {
         return new Promise((resolve, reject) => {
             // wrap in timeout to simulate server api call
             setTimeout(() => {
-
                 // authenticate
-                if (url.endsWith('/users/authenticate') && opts.method === 'POST') {
+                if (
+                    url.endsWith("/users/authenticate") &&
+                    opts.method === "POST"
+                ) {
                     // get parameters from post request
                     let params = JSON.parse(opts.body);
 
                     // find if any user matches login credentials
-                    let filteredUsers = users.filter(user => {
-                        return user.username === params.username && user.password === params.password;
+                    let filteredUsers = users.filter((user) => {
+                        return (
+                            user.username === params.username &&
+                            user.password === params.password
+                        );
                     });
 
                     if (filteredUsers.length) {
@@ -26,66 +31,89 @@ export function configureFakeBackend() {
                             username: user.username,
                             firstName: user.firstName,
                             lastName: user.lastName,
-                            token: 'fake-jwt-token'
+                            token: "fake-jwt-token",
                         };
-                        resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(responseJson)) });
+                        resolve({
+                            ok: true,
+                            text: () =>
+                                Promise.resolve(JSON.stringify(responseJson)),
+                        });
                     } else {
                         // else return error
-                        reject('Username or password is incorrect');
+                        reject("Username or password is incorrect");
                     }
 
                     return;
                 }
 
                 // get users
-                if (url.endsWith('/users') && opts.method === 'GET') {
+                if (url.endsWith("/users") && opts.method === "GET") {
                     // check for fake auth token in header and return users if valid, this security is implemented server side in a real application
-                    if (opts.headers && opts.headers.Authorization === 'Bearer fake-jwt-token') {
-                        resolve({ ok: true, text: () => Promise.resolve(JSON.stringify(users))});
+                    if (
+                        opts.headers &&
+                        opts.headers.Authorization === "Bearer fake-jwt-token"
+                    ) {
+                        resolve({
+                            ok: true,
+                            text: () => Promise.resolve(JSON.stringify(users)),
+                        });
                     } else {
                         // return 401 not authorised if token is null or invalid
-                        reject('Unauthorised');
+                        reject("Unauthorised");
                     }
 
                     return;
                 }
 
                 // get user by id
-                if (url.match(/\/users\/\d+$/) && opts.method === 'GET') {
+                if (url.match(/\/users\/\d+$/) && opts.method === "GET") {
                     // check for fake auth token in header and return user if valid, this security is implemented server side in a real application
-                    if (opts.headers && opts.headers.Authorization === 'Bearer fake-jwt-token') {
+                    if (
+                        opts.headers &&
+                        opts.headers.Authorization === "Bearer fake-jwt-token"
+                    ) {
                         // find user by id in users array
-                        let urlParts = url.split('/');
+                        let urlParts = url.split("/");
                         let id = parseInt(urlParts[urlParts.length - 1]);
-                        let matchedUsers = users.filter(user => { return user.id === id; });
+                        let matchedUsers = users.filter((user) => {
+                            return user.id === id;
+                        });
                         let user = matchedUsers.length ? matchedUsers[0] : null;
 
                         // respond 200 OK with user
-                        resolve({ ok: true, text: () => JSON.stringify(user)});
+                        resolve({ ok: true, text: () => JSON.stringify(user) });
                     } else {
                         // return 401 not authorised if token is null or invalid
-                        reject('Unauthorised');
+                        reject("Unauthorised");
                     }
 
                     return;
                 }
 
                 // register user
-                if (url.endsWith('/users/register') && opts.method === 'POST') {
+                if (url.endsWith("/users/register") && opts.method === "POST") {
                     // get new user object from post body
                     let newUser = JSON.parse(opts.body);
 
                     // validation
-                    let duplicateUser = users.filter(user => { return user.username === newUser.username; }).length;
+                    let duplicateUser = users.filter((user) => {
+                        return user.username === newUser.username;
+                    }).length;
                     if (duplicateUser) {
-                        reject('Username "' + newUser.username + '" is already taken');
+                        reject(
+                            'Username "' +
+                                newUser.username +
+                                '" is already taken'
+                        );
                         return;
                     }
 
                     // save new user
-                    newUser.id = users.length ? Math.max(...users.map(user => user.id)) + 1 : 1;
+                    newUser.id = users.length
+                        ? Math.max(...users.map((user) => user.id)) + 1
+                        : 1;
                     users.push(newUser);
-                    localStorage.setItem('users', JSON.stringify(users));
+                    localStorage.setItem("users", JSON.stringify(users));
 
                     // respond 200 OK
                     resolve({ ok: true, text: () => Promise.resolve() });
@@ -94,18 +122,24 @@ export function configureFakeBackend() {
                 }
 
                 // delete user
-                if (url.match(/\/users\/\d+$/) && opts.method === 'DELETE') {
+                if (url.match(/\/users\/\d+$/) && opts.method === "DELETE") {
                     // check for fake auth token in header and return user if valid, this security is implemented server side in a real application
-                    if (opts.headers && opts.headers.Authorization === 'Bearer fake-jwt-token') {
+                    if (
+                        opts.headers &&
+                        opts.headers.Authorization === "Bearer fake-jwt-token"
+                    ) {
                         // find user by id in users array
-                        let urlParts = url.split('/');
+                        let urlParts = url.split("/");
                         let id = parseInt(urlParts[urlParts.length - 1]);
                         for (let i = 0; i < users.length; i++) {
                             let user = users[i];
                             if (user.id === id) {
                                 // delete user
                                 users.splice(i, 1);
-                                localStorage.setItem('users', JSON.stringify(users));
+                                localStorage.setItem(
+                                    "users",
+                                    JSON.stringify(users)
+                                );
                                 break;
                             }
                         }
@@ -114,16 +148,15 @@ export function configureFakeBackend() {
                         resolve({ ok: true, text: () => Promise.resolve() });
                     } else {
                         // return 401 not authorised if token is null or invalid
-                        reject('Unauthorised');
+                        reject("Unauthorised");
                     }
 
                     return;
                 }
 
                 // pass through any requests not handled above
-                realFetch(url, opts).then(response => resolve(response));
-
+                realFetch(url, opts).then((response) => resolve(response));
             }, 500);
         });
-    }
+    };
 }
