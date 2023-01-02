859adb21ce7a65630de6d53229f4938cc2f79ccb
nafihpp <nafihpp@gmail.com>
2023-01-02 20:00:53 +0530

    update

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { userActions } from '../_actions';
+import { userActions } from "../_actions";
 
 class HomePage extends React.Component {
     componentDidMount() {
@@ -21,21 +21,38 @@ class HomePage extends React.Component {
                 <p>You're logged in with React!!</p>
                 <h3>All registered users:</h3>
                 {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
+                {users.error && (
+                    <span className="text-danger">ERROR: {users.error}</span>
+                )}
+                {users.items && (
                     <ul>
-                        {users.items.map((user, index) =>
+                        {users.items.map((user, index) => (
                             <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
+                                {user.firstName + " " + user.lastName}
+                                {user.deleting ? (
+                                    <em> - Deleting...</em>
+                                ) : user.deleteError ? (
+                                    <span className="text-danger">
+                                        {" "}
+                                        - ERROR: {user.deleteError}
+                                    </span>
+                                ) : (
+                                    <span>
+                                        {" "}
+                                        -{" "}
+                                        <a
+                                            onClick={this.handleDeleteUser(
+                                                user.id
+                                            )}
+                                        >
+                                            Delete
+                                        </a>
+                                    </span>
+                                )}
                             </li>
-                        )}
+                        ))}
                     </ul>
-                }
+                )}
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
@@ -52,8 +69,8 @@ function mapState(state) {
 
 const actionCreators = {
     getUsers: userActions.getAll,
-    deleteUser: userActions.delete
-}
+    deleteUser: userActions.delete,
+};
 
 const connectedHomePage = connect(mapState, actionCreators)(HomePage);
 export { connectedHomePage as HomePage };
